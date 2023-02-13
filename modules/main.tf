0ae9acbd72b51d6972155d6c9496475277fad174
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-02-12 23:56:19 -0500

    PLease

--- modules/main.tf
+++ modules/main.tf
@@ -1,9 +1,9 @@
 module "networking" {
-  source = "./networking"
-  region = var.region
-  profile = var.profile
-  availability_zones = var.availability_zones
-  vpc_cidr = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
+  source              = "./networking"
+  region              = var.region
+  profile             = var.profile
+  availability_zones  = var.availability_zones
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
 }
