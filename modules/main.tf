a647e8ed30dc9fe58107df052ec4facb57deb3b1
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-02-12 23:12:54 -0500

    Fail

--- modules/main.tf
+++ modules/main.tf
@@ -1,7 +1,7 @@
 module "networking" {
   source = "./networking"
   region = var.region
-  profile = var.profile
+  profile = var.  profile
   availability_zones = var.availability_zones
   vpc_cidr = var.vpc_cidr
   public_subnet_cidr = var.public_subnet_cidr
