24965928563cb821051a3352e2a4b0a501e5dd7d
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-02-21 13:17:05 -0500

    Final

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -85,13 +85,20 @@ resource "aws_security_group" "application_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "webserver" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.application_sg.id]
-  subnet_id     = aws_subnet.public_subnet[1].id
+  subnet_id     = aws_subnet.public_subnet[0].id
   associate_public_ip_address = true
   root_block_device {
     volume_size = 50
