218604e5b70e9d8be8b2303cc42ce859486409c5
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-02-21 23:39:32 -0500

    Added terraform fmt

--- modules/main.tf
+++ modules/main.tf
@@ -6,5 +6,5 @@ module "networking" {
   vpc_cidr            = var.vpc_cidr
   public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  ami_id = var.ami_id
+  ami_id              = var.ami_id
 }
